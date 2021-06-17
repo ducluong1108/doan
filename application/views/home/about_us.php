7fc8dfe75c2519ffbed57a0be41ca16adf82b0d3
hoangloc1307 <hoangloc1307@gmail.com>
2021-06-17 13:09:18 +0700

    Ngay 3: Tiep tuc trang ve chung toi

--- application/views/home/about_us.php
+++ application/views/home/about_us.php
@@ -1,7 +1,8 @@
 <div id="aboutus">
     <h2> Về chúng tôi </h2>
-    <div class="container">
-        <section class="introduce">
+
+    <section class="introduce">
+        <div class="container">
             <div class="content">
                 <h3>Chúng tôi là <span>Xây dựng Hoàng Đức</span></h3>
                 <p>
@@ -35,6 +36,98 @@
             <div class="image">
                 <img src="<?= base_url('assets/images/tonggiamdoc.jpg') ?>" alt="Tổng giám đốc">
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
+    <section class="grown_up">
+        <div class="container">
+            <h3>Quá trình phát triển</h3>
+            <div class="grown_content">
+                <div class="item">
+                    <div>
+                        <p>Thành lập công ty First Green Engineering với chức năng chính là nhà tư vấn, nhà thầu cơ điện
+                        </p>
+                    </div>
+                    <span>2012</span>
+                    <div>
+                        <img src="<?= base_url('assets/images/grown1.jpg') ?>">
+                    </div>
+                </div>
+                <div class="item">
+                    <div>
+                        <p>Thực hiện gói thầu Cơ Điện dự án Eximbank Data Center tại Bình Dương</p>
+                    </div>
+                    <span>2015</span>
+                    <div>
+                        <img src="<?= base_url('assets/images/grown2.jpg') ?>">
+                    </div>
+                </div>
+                <div class="item">
+                    <div>
+                        <p>Thành lập công ty thành viên Green Mark Engineering với chức năng là nhà thầu thi công Cơ
+                            Điện.
+                            Thực hiện một số các dự án trong lĩnh vực công nghiệp như Nhà máy nước mắm Nam Ngư Phú Quốc,
+                            Nhà
+                            máy dược OPV, Nhà máy Pepsico Hóc Môn, Nhà máy nước khoáng Vĩnh Hảo, Nhà máy thực phẩm Việt
+                            Tiến, Xưởng may tại KCN Long Hậu, ...</p>
+                    </div>
+                    <span>2016</span>
+                    <div>
+                        <img src="<?= base_url('assets/images/grown3.jpg') ?>">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="finance">
+        <div class="container">
+            <h3>Nguồn lực tài chính</h3>
+            <p>
+                First Green Engineering đang từng bước xây dựng đội ngũ nhân viên có chuyên môn cao cũng như tạo lập kế
+                hoạch tài chính hợp lý để thúc đẩy các hoạt động kinh doanh mang tính ổn định và phát triển bền vững.
+            </p>
+            <div class="image">
+                <div class="left">
+                    <img src="<?= base_url('assets/images/finance1.png') ?>">
+                </div>
+                <div class="right">
+                    <img src="<?= base_url('assets/images/finance2.png') ?>">
+                    <img src="<?= base_url('assets/images/finance3.png') ?>">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="misson">
+        <div class="container">
+            <div class="card">
+                <img src="<?= base_url('assets/images/tam_nhin.png') ?>">
+                <h3>Tầm nhìn</h3>
+                <p>Chúng tôi hướng đến mục tiêu phát triển bền vững và trở thành những chuyên gia hàng đầu tại Việt Nam
+                    trong lĩnh vực Cơ điện</p>
+            </div>
+            <div class="card">
+                <img src="<?= base_url('assets/images/su_menh.png') ?>">
+                <h3>Sứ mệnh</h3>
+                <p>Tạo dựng môi trường làm việc lấy nhân viên là nòng cốt cho sự phát triển. Thúc đẩy sự sang tạo và
+                    mang lại những giá trị thực cho khách hàng</p>
+            </div>
+            <div class="card">
+                <img src="<?= base_url('assets/images/gia_tri_cot_loi.png') ?>">
+                <h3>Tầm nhìn</h3>
+                <table>
+                    <tr>
+                        <td>Nhân văn</td>
+                        <td>Chất lượng</td>
+                    </tr>
+                    <tr>
+                        <td>Tin cậy</td>
+                        <td>Sáng tạo</td>
+                    </tr>
+                    <tr>
+                        <td>Cam kết</td>
+                        <td>Hiệu quả</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </section>
 </div>
